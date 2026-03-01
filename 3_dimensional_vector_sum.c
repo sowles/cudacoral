c7663bff14a3a08bfe7a1a40cf92092a0f2c84a9
George Sowles <sowlesgeorge@gmail.com>
2026-02-28 21:10:06 -0500

    better io

--- 3_dimensional_vector_sum.c
+++ 3_dimensional_vector_sum.c
@@ -14,14 +14,19 @@ int main(int argc, char* argv[]) {
 	float* B_h = (float*)malloc(n * sizeof(float));
 	float* C_h = (float*)malloc(n * sizeof(float));
 
-	for (int i = 0; i < n; ++i)
+	for (int i = 0; i < n; ++i) {
+		printf("A dimension %d: ", i + 1);
 		scanf("%f", &A_h[i]);
+	}
 
-	for (int i = 0; i < n; ++i)
+	for (int i = 0; i < n; ++i) {
+		printf("B dimension %d: ", i + 1);
 		scanf("%f", &B_h[i]);
+	}
 
 	vecAdd(A_h, B_h, C_h, n);
 
+	printf("Result: ");
 	for (int i = 0; i < n; ++i)
 		printf("%f ", C_h[i]);
 	printf("\n");
